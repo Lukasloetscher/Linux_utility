3d95c207d6a17f10fc325c33f17cb81d5e1e5269
LukasLoetscher <lukas@pop-os.home>
2025-04-04 16:49:08 +0200

    added script which can be used when i need to reinstall my os.

---
+++ initial_setup_for_new_linux_installation.sh
@@ -0,0 +1,35 @@
+apt update
+apt install snapd -y
+snap install eclipse --classic
+
+apt install virtualbox -y
+
+apt install openjdk-17-jdk -y
+apt install maven -y
+
+# Add environment variables to ~/.profile
+echo '' >> ~/.profile
+echo '# >>> Java & Maven environment variables >>>' >> ~/.profile
+echo 'export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))' >> ~/.profile
+echo 'export MAVEN_HOME=/usr/share/maven' >> ~/.profile
+echo 'export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH' >> ~/.profile
+echo '# <<< Java & Maven environment variables <<<' >> ~/.profile
+
+apt install code -y
+apt install golang -y
+
+
+#adding extensions to vs code
+code --install-extension golang.go
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.cpptools
+
+apt install docker.io -y
+
+ssh-keygen -t ed25519 -C "lukas@loetscher.swiss" -f /home/$USER/.ssh/github_ssh_key -N ""
+cp /home/$USER/.ssh/github_ssh_key.pub /home/$USER/Desktop/ssh_key_for_github.pub
+echo "please paste the pub key into github."
+
+
+
+
