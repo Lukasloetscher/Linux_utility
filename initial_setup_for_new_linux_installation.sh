94e13f3965e2e59c267093e30697153c9403747f
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-04-06 11:32:35 +0200

    restructered the file, fo rbetter readability

--- initial_setup_for_new_linux_installation.sh
+++ initial_setup_for_new_linux_installation.sh
@@ -1,42 +1,96 @@
 #!/bin/bash
 
-apt update
-apt install snapd -y
-snap install eclipse --classic
+        func_AptBasics(){
+            apt update
+            apt install snapd -y
+        }
 
-apt install virtualbox -y
-
-apt install openjdk-17-jdk -y
-apt install maven -y
-
-# Add environment variables to ~/.profile
-echo '' >> ~/.profile
-echo '# >>> Java & Maven environment variables >>>' >> ~/.profile
-echo 'export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))' >> ~/.profile
-echo 'export MAVEN_HOME=/usr/share/maven' >> ~/.profile
-echo 'export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH' >> ~/.profile
-echo '# <<< Java & Maven environment variables <<<' >> ~/.profile
-
-apt install code -y
-apt install golang -y
+        func_vmBox(){
+            apt install virtualbox -y
+        }
 
 
-#adding extensions to vs code
-code --install-extension golang.go
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-vscode.cpptools
-code --install-extension foxundermoon.shell-format
+                
+                func_javaSetupIDE(){
+                    apt install snapd -y
+                    snap install eclipse --classic
+
+                }
+
+                func_javaSetUpProfile(){
+                    echo '' >> ~/.profile
+                    echo '# >>> Java & Maven environment variables >>>' >> ~/.profile
+                    echo 'export Jusermod -aG docker $USER'
+                }
+
+                func_javaSetuputility(){
+                    apt install openjdk-17-jdk -y
+                    apt install maven -y
+
+                    func_javaSetUpProfile()
+                } 
+
+        func_javaSetup(){
+            func_javaSetupIDE()
+            func_javaSetuputility()
+        }
 
 
-apt install docker.io -y
+                func_vscodeExtensions(){ #Those are grouped together here, since they are dependent on vscode beeing installed. Even so there could be an argument, that they should be under their respective tools.
+                    code --install-extension golang.go
+                    code --install-extension ms-azuretools.vscode-docker
+                    code --install-extension ms-vscode.cpptools
+                    code --install-extension foxundermoon.shell-format
 
-ssh-keygen -t ed25519 -C "lukas@loetscher.swiss" -f /home/$USER/.ssh/github_ssh_key -N ""
-cp /home/$USER/.ssh/github_ssh_key.pub /home/$USER/Desktop/ssh_key_for_github.pub
-echo "please paste the pub key into github."
 
-#git set up.
-git config --global user.name "Lukas Loetscher"
-git config --global user.email "Lukas@Loetscher.swiss"
-#makes it teh default that git wants to merge stuff in case of missing changes.
-git config pull.rebase false 
+                }
 
+        func_vscode(){
+            apt install code -y
+            func_vscodeExtensions()
+        }
+
+        func_go(){
+            apt install golang -y
+        }
+
+        func_Docker(){
+            apt install docker.io -y
+
+            #setting docker access. needed to be used with vscode.
+            usermod -aG docker $USER
+
+        }
+
+                func_GitSettings(){
+                    #git set up.
+                    git config --global user.name "Lukas Loetscher"
+                    git config --global user.email "Lukas@Loetscher.swiss"
+                    #makes it teh default that git wants to merge stuff in case of missing changes.
+                    git config pull.rebase false 
+                }
+
+                func_GIT_SSHKeyForGithub(){
+                    ssh-keygen -t ed25519 -C "lukas@loetscher.swiss" -f /home/$USER/.ssh/github_ssh_key -N ""
+                    p /home/$USER/.ssh/github_ssh_key.pub /home/$USER/Desktop/ssh_key_for_github.pub
+                    cho "please paste the pub key into github."
+                }
+
+        func_GIT(){
+            #git is installed as a standart, however there are some settings needed.
+            func_GitSettings()
+            func_GIT_SSHKeyForGithub()
+        }
+
+#THIS IS WHERE THE CODE STARTS!
+
+func_AptBasics()
+func_vmBox()
+func_javaSetup()
+func_go()
+func_vscode()
+func_Docker()
+func_GIT()
+
+#last we want to reboot the device.
+reboot now
